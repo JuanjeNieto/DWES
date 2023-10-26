f772683ffa18a0339d3f56575984e1ec797a137e
JuanjeNieto <juanjenieto3107@gmail.com>
2023-10-26 23:48:43 +0200

    A

---
+++ EjerciciosLJS/Tema2 - Formularios/Ejercicio1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- Realiza un programa que pida dos números y luego muestre el resultado de su multiplicación.
+ -->
+    <form action="suma.php" method="get">
+    Primer número: <input type="number" name="a"><br>
+    Segundo número: <input type="number" name="b"><br>
+    <input type="submit" value="Sumar">
+
+</body>
+</html>
