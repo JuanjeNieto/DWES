e0db6c2953a98a44f2c887ecf689507d7b41aa9f
JuanjeNieto <juanjenieto3107@gmail.com>
2023-10-26 23:53:13 +0200

    asas

--- EjerciciosLJS/Tema2 - Formularios/Ejercicio1/suma.php
+++ EjerciciosLJS/Tema2 - Formularios/Ejercicio1/suma.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-        <meta charset="UTF-8">
-        </head>
-        <body>
-            <?php
-                $a = $_GET['a'];
-                $b = $_GET['b'];
-                echo "La suma de $a mas $b es ", $a + $b;
-            ?>
-        </body>
-    </html>
+
+<?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    echo "La suma de $a mas $b es ", $a + $b;
+?>
+       
