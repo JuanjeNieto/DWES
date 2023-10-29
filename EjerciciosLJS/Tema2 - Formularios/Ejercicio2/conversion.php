d523f7c36e1aa983df50994650013312c52434dd
JuanjeNieto <juanjenieto3107@gmail.com>
2023-10-29 23:32:06 +0100

    seguir ej LJS

---
+++ EjerciciosLJS/Tema2 - Formularios/Ejercicio2/conversion.php
@@ -0,0 +1,5 @@
+<?php 
+ $euros = $_GET['euros'];
+
+ echo "La cantidad de $euros a peseta son: ",$euros * 166 ; 
+?>
---
+++ EjerciciosLJS/Tema2 - Formularios/Ejercicio2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="conversion.php" method="get">
+    Inserte cantidad en euros: 
+    <input type="number" name="euros">
+    <input type="submit" value="aPeseta">
+    </form>
+</body>
+</html>
