bec04ddcff51dbbdf6016fcdf1e6f1b3852a975e
JuanjeNieto <juanjenieto3107@gmail.com>
2023-10-30 23:23:38 +0100

    asas

--- EjerciciosLJS/Tema3 - Sentencias/index.php
+++ EjerciciosLJS/Tema3 - Sentencias/index.php
@@ -6,9 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+    <form method="get">
+        <p>Inserta el dia de la semana (lunes a viernes)</p>
+        <input type="text" name="dia">
+        <input type="submit" value="Asignatura">
+    </form>
     <?php 
-    
-    
+    $dia = $_GET['dia'];
+    switch ($dia) {
+        case 'lunes':
+            echo "Toca DWES el día $dia a primera hora";
+            break;
+        
+        default:
+            # code...
+            break;
+    }
     
     ?>
 </body>
