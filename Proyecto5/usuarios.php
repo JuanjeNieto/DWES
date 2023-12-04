206859a602ab91e8396730a2d277369282ce612e
JuanjeNieto <juanjenieto3107@gmail.com>
2023-12-04 23:10:56 +0100

    empezar p5

---
+++ Proyecto5/usuarios.php
@@ -0,0 +1,28 @@
+<?php
+
+class Usuarios {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function validarUsuario($username, $password) {
+        
+    }
+
+    public function altaUsuario($username, $password, $fullName) {
+        
+    }
+
+    public function modificarUsuario($username, $newData) {
+       
+    }
+
+    public function borrarUsuario($username) {
+        
+    }
+}
+
+?>
+
